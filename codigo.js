91cafc845edd48f85ec74c3a2e36d24508eff84d
jose <gomezjosearmando406@gmail.com>
2025-05-13 20:46:45 -0300

    version tablas

--- codigo.js
+++ codigo.js
@@ -1,5 +1,33 @@
-var x = 1;
-        while(x < 5){
-            x = x + 1;
-            console.log(x);
-        }
+function mostrar(){
+
+    var dato = document.getElementById("entrada1");
+
+    var resultado = document.getElementById("resultado");
+
+   // resultado.innerText = dato.value;
+
+   var r2 = dato.value;
+
+   
+
+
+    var contador = 1;
+    while (contador < 13) {
+
+        r2 = dato.value * contador;
+         
+        //resultado.innerText = resultado.innerText  + contador + "\n";
+        resultado.innerText +=   dato.value +' x ' + contador + ' = ' + r2 +  "\n";
+       // contador = contador + 1;
+        contador++
+
+        
+    }
+    
+    
+    
+
+
+}
+
+document.getElementById("boton1").onclick = mostrar;
